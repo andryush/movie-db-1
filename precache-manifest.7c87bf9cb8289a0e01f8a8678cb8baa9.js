a2d8595dd198895144f3370e81fec7d58c14bbe1
andryush <andryush333@gmail.com>
2020-07-28 04:49:44 +0400

    Updates

---
+++ precache-manifest.7c87bf9cb8289a0e01f8a8678cb8baa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a2cbd1c49824bce6f4c44eeefcc8e4",
+    "url": "/movie-db-1/index.html"
+  },
+  {
+    "revision": "52c90af73fc19c6864d5",
+    "url": "/movie-db-1/static/css/main.3fb730a1.chunk.css"
+  },
+  {
+    "revision": "0c0b6d852d92c40d44aa",
+    "url": "/movie-db-1/static/js/2.1a617aa2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-db-1/static/js/2.1a617aa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52c90af73fc19c6864d5",
+    "url": "/movie-db-1/static/js/main.aabaaed4.chunk.js"
+  },
+  {
+    "revision": "e6e51e8fc8d2d65689ec",
+    "url": "/movie-db-1/static/js/runtime-main.caa037c5.js"
+  }
+]);
